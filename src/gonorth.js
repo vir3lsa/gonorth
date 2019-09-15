c2c59623a430011acfa60c97a7d90c838e30f7e5
Rich Locke <virelsa@pilchardo.com>
2019-09-15 21:32:04 +0100

    Print the game title when the game runs

--- src/gonorth.js
+++ src/gonorth.js
@@ -1,9 +1,23 @@
 import Game from './game';
+import { output } from './output';
 
-export default {
+const createGame = title => {
+    return new Game(title);
+};
 
-    createGame: title => {
-        return new Game(title);
+const playGame = game => {
+    if (!game) {
+        console.error('No game to play');
     }
 
+    output(formatTitle(game.title || 'Untitled'));
+};
+
+const formatTitle = title => [...title].map((c, i) => {
+    return `${c}${i === title.length - 1 ? '' : ' '}`;
+}).join('');
+
+export default {
+    createGame,
+    playGame
 }
