ad45a8ed2cfe88889fdf9699dd6aad686a16a4be
Rich Locke <virelsa@pilchardo.com>
2020-01-15 22:20:12 +0000

    Add input history

--- src/web/parserBar.js
+++ src/web/parserBar.js
@@ -3,13 +3,64 @@ import TextField from "@material-ui/core/TextField";
 import { receiveInput } from "../utils/inputReceiver";
 import { reactionTimePassed } from "../utils/sharedFunctions";
 
+let historyIndex = 0;
+let history = [];
+const maxHistory = 500;
+
+history[0] = "";
+
 const captureInput = event => {
   if (event.key === "Enter" && reactionTimePassed()) {
-    receiveInput(event.target.value);
+    const value = event.target.value;
+    receiveInput(value);
 
     // Reset input
     event.target.value = "";
+
+    // Record history
+    if (!history.length || value !== history[1]) {
+      history.unshift(value);
+      history[1] = value; // In case we didn't have a working value
+    }
+
+    if (history.length > maxHistory + 1) {
+      history = history.slice(0, maxHistory + 1);
+    }
+
+    // Blank the working value
+    history[0] = "";
+    historyIndex = 0;
   }
+};
+
+const handleChange = event => {
+  const value = event.target.value;
+
+  if (historyIndex > 0 && value !== history[historyIndex]) {
+    // We've edited a history item - it's our new working value
+    historyIndex = 0;
+  }
+
+  if (historyIndex === 0) {
+    // Record our new working value
+    history[0] = value;
+  }
+};
+
+const handleArrows = event => {
+  if (event.key === "ArrowUp" && historyIndex < history.length - 1) {
+    historyIndex++;
+  } else if (event.key === "ArrowDown" && historyIndex > 0) {
+    historyIndex--;
+  } else {
+    return;
+  }
+
+  const textField = event.target;
+  textField.value = history[historyIndex];
+  textField.selectionStart = textField.value.length;
+  textField.selectionEnd = textField.value.length;
+  event.preventDefault();
 };
 
 export const ParserBar = props => {
@@ -24,7 +75,9 @@ export const ParserBar = props => {
       placeholder="What do you want to do?"
       fullWidth
       margin="normal"
+      onKeyDown={handleArrows}
       onKeyUp={captureInput}
+      onChange={handleChange}
       inputRef={inputRef}
       autoComplete="off"
     />
