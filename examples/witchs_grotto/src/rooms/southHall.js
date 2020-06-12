e81d51212553ccc74bcfc23160514aff2e661924
Rich Locke <virelsa@pilchardo.com>
2020-06-12 23:48:29 +0100

    Curving corridor basics

--- examples/witchs_grotto/src/rooms/southHall.js
+++ examples/witchs_grotto/src/rooms/southHall.js
@@ -1,8 +1,23 @@
-import { Room } from "../../../../lib/gonorth";
+import { Room, Door } from "../../../../lib/gonorth";
 import { bedroom } from "./bedroom";
 import { upperSpiral } from "./upperSpiral";
 
 export const southHall = new Room("South Hall", "placeholder");
 
+const ricketyDoor = new Door(
+  "rickety door",
+  "A rickety looking wooden door consisting of barely held-together rotten planks. One hinge is missing - the others appear to be in working order.",
+  false,
+  false
+);
+
+southHall.addItems(ricketyDoor);
+upperSpiral.addItems(ricketyDoor);
+
 southHall.setEast(bedroom);
-southHall.setSouth(upperSpiral);
+southHall.setSouth(
+  upperSpiral,
+  () => ricketyDoor.open,
+  "You step through the doorway.",
+  "Rickety as it may be, it's still a door, and it's still closed. Much as you'd like to, you can't simply pass through it."
+);
