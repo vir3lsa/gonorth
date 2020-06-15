7611c63088bfdc759e9485f83c872d685e02655c
richlocke87 <github@richardlocke.co.uk>
2020-06-15 08:12:54 +0100

    Test commit from CodeSandbox

--- examples/witchs_grotto/src/rooms/diningRoom.js
+++ examples/witchs_grotto/src/rooms/diningRoom.js
@@ -3,7 +3,12 @@ import { nook } from "./nook";
 
 export const diningRoom = new Room(
   "Dining Room",
-  "The room is long and thin and is dominated by a large, if plain, table, marking this out as the dining room. Several candles flicker gently at evenly-spaced intervals along the table. Three places have been set with plates, cutlery and large wine goblets. Is the witch expecting guests?\n\nAt the rear of the room, a bead curtain with a moon and stars motif leads Westwards, and an archway opens onto the kitchen to the East."
+  "The room is long and thin and is dominated by a large, if plain, table, marking this out as the dining room. Several candles flicker gently at evenly-spaced intervals along the table. Three places have been set with plates, cutlery and large wine goblets. Is the witch expecting guests?\n\nDown one wall is a long wooden cabinet, and various pictures adorn the walls.\n\nAt the rear of the room, a bead curtain with a moon and stars motif leads Westwards, and an archway opens onto the kitchen to the East."
+);
+
+const pictures = new Item(
+  "pictures",
+  "There's a disturbing oil painting depicting a stern-looking woman standing over a child who's fallen in the mud and an incongruously cheery cross-stitch. The others don't particularly catch your eye."
 );
 
 const beadCurtain = new Item(
@@ -14,6 +19,8 @@ beadCurtain.aliases = ["curtain", "beads", "moon and stars", "moon", "stars"];
 
 diningRoom.addItem(beadCurtain);
 nook.addItem(beadCurtain);
+
+diningRoom.add(pictures);
 
 diningRoom.setWest(
   nook,
