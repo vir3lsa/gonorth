f06d377d4b10b340c7e3ba7f4578706770deb2fc
Rich Locke <virelsa@pilchardo.com>
2021-10-15 23:19:18 +0100

    Debounce scroll

--- src/web/iodevice.js
+++ src/web/iodevice.js
@@ -2,23 +2,25 @@ import React, { useEffect, useRef } from "react";
 import ReactMarkdown from "react-markdown";
 import { connect } from "react-redux";
 import { Element, scroller } from "react-scroll";
+import { debounce } from "debounce";
 import { DecisionBar } from "./decisionBar";
 import { ParserBar } from "./parserBar";
 import { SceneImage } from "./sceneImage";
 
+const debouncedScroll = debounce(
+  () =>
+    scroller.scrollTo("scrollTarget", {
+      smooth: "easeInQuad",
+      duration: 1500,
+      containerId: "scrollPane",
+      ignoreCancelEvents: true
+    }),
+  10
+);
+
 const IODevice = (props) => {
   const { interaction } = props;
-
-  useEffect(
-    () =>
-      scroller.scrollTo("scrollTarget", {
-        smooth: "easeInQuad",
-        duration: 1500,
-        containerId: "scrollPane",
-        ignoreCancelEvents: true
-      }),
-    [interaction.currentPage]
-  );
+  useEffect(debouncedScroll, [interaction.currentPage]);
 
   return (
     <div
