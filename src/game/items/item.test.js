4ee319c0f4dc8aedd9bc38cad06f48c19e5a0389
Rich Locke <virelsa@pilchardo.com>
2022-05-09 17:38:51 +0100

    Fix Promise error output in tests

--- src/game/items/item.test.js
+++ src/game/items/item.test.js
@@ -6,7 +6,7 @@ import { newGame, recordChanges } from "../../redux/gameActions";
 import { selectActionChainPromise, selectInventory } from "../../utils/selectors";
 import { Room } from "./room";
 import { initGame, setInventoryCapacity } from "../../gonorth";
-import { selectCurrentPage, selectInteraction, selectOptions } from "../../utils/testSelectors";
+import { selectCurrentPage, selectOptions } from "../../utils/testSelectors";
 import { Container } from "./container";
 import { Verb } from "../../../src/game/verbs/verb";
 import { clickNextAndWait } from "../../utils/testFunctions";
@@ -28,93 +28,101 @@ beforeEach(() => {
   getStore().dispatch(newGame(game, true, false));
 });
 
-test("items can have variable descriptions", () => {
-  let looks = 0;
-  const clock = new Item("clock", (item) => {
-    looks++;
-    if (looks === 1) {
-      return "It's 12 o'clock";
-    } else if (looks === 2) {
-      return "It's 1 o'clock";
-    }
+describe("basic item tests", () => {
+  test("items can have variable descriptions", () => {
+    let looks = 0;
+    const clock = new Item("clock", (item) => {
+      looks++;
+      if (looks === 1) {
+        return "It's 12 o'clock";
+      } else if (looks === 2) {
+        return "It's 1 o'clock";
+      }
+    });
+
+    expect(clock.description).toBe("It's 12 o'clock");
+    expect(clock.description).toBe("It's 1 o'clock");
   });
 
-  expect(clock.description).toBe("It's 12 o'clock");
-  expect(clock.description).toBe("It's 1 o'clock");
-});
+  it("Doesn't render a Next button for cyclic descriptions", () => {
+    const table = new Item("table", ["It's made of wood.", "It has four legs."]);
+    table.try("x");
+    expect(selectOptions()).toBeNull();
+  });
 
-it("Doesn't render a Next button for cyclic descriptions", () => {
-  const table = new Item("table", ["It's made of wood.", "It has four legs."]);
-  table.try("x");
-  expect(selectOptions()).toBeNull();
-});
+  it("renders each page of sequential text then stops", async () => {
+    const chair = new Item("chair", new SequentialText("a", "b"));
+    chair.try("x");
+    const clickNextPromise = new Promise((resolve) =>
+      setTimeout(async () => {
+        await clickNextAndWait();
+        resolve();
+      })
+    );
+    expect(selectOptions()).toBeNull();
+    return clickNextPromise;
+  });
 
-it("renders each page of sequential text then stops", async () => {
-  const chair = new Item("chair", new SequentialText("a", "b"));
-  chair.try("x");
-  setTimeout(async () => await clickNextAndWait());
-  expect(selectOptions()).toBeNull();
-});
+  test("items can be picked up", async () => {
+    const watch = new Item("watch", "posh-looking", true, 1);
+    room.addItem(watch);
+    await watch.try("take");
+    expect(selectInventory().items.watch[0].name).toBe("watch");
+  });
 
-test("items can be picked up", async () => {
-  const watch = new Item("watch", "posh-looking", true, 1);
-  room.addItem(watch);
-  await watch.try("take");
-  expect(selectInventory().items.watch[0].name).toBe("watch");
-});
+  test("items can't be picked up if they're bigger than the inventory", async () => {
+    setInventoryCapacity(5);
+    const medicineBall = new Item("medicine ball", "big", true, 6);
+    room.addItem(medicineBall);
+    await medicineBall.try("take");
+    expect(selectInventory().items["medicine ball"]).toBeUndefined();
+    expect(selectCurrentPage()).toInclude("don't have enough room");
+  });
 
-test("items can't be picked up if they're bigger than the inventory", async () => {
-  setInventoryCapacity(5);
-  const medicineBall = new Item("medicine ball", "big", true, 6);
-  room.addItem(medicineBall);
-  await medicineBall.try("take");
-  expect(selectInventory().items["medicine ball"]).toBeUndefined();
-  expect(selectCurrentPage()).toInclude("don't have enough room");
-});
+  test("items can't be picked up if there's no room left", async () => {
+    setInventoryCapacity(10);
+    const medicineBall = new Item("medicine ball", "big", true, 6);
+    const panda = new Item("panda", "black and white", true, 6);
+    room.addItem(medicineBall);
+    room.addItem(panda);
+    await medicineBall.try("take");
+    await panda.try("take");
+    expect(selectInventory().items["medicine ball"]).not.toBeUndefined();
+    expect(selectInventory().items.panda).toBeUndefined();
+    expect(selectCurrentPage()).toInclude("don't have enough room");
+  });
 
-test("items can't be picked up if there's no room left", async () => {
-  setInventoryCapacity(10);
-  const medicineBall = new Item("medicine ball", "big", true, 6);
-  const panda = new Item("panda", "black and white", true, 6);
-  room.addItem(medicineBall);
-  room.addItem(panda);
-  await medicineBall.try("take");
-  await panda.try("take");
-  expect(selectInventory().items["medicine ball"]).not.toBeUndefined();
-  expect(selectInventory().items.panda).toBeUndefined();
-  expect(selectCurrentPage()).toInclude("don't have enough room");
-});
+  test("items can't be picked up twice", async () => {
+    const cup = new Item("cup", "little", true, 1);
+    room.addItem(cup);
+    await cup.try("take");
+    await cup.try("take");
+    expect(selectCurrentPage()).toInclude("already carrying");
+  });
 
-test("items can't be picked up twice", async () => {
-  const cup = new Item("cup", "little", true, 1);
-  room.addItem(cup);
-  await cup.try("take");
-  await cup.try("take");
-  expect(selectCurrentPage()).toInclude("already carrying");
-});
+  test("items can be built with a builder", () => {
+    const pipe = new Item.Builder()
+      .withName("pipe")
+      .withDescription("This is not a pipe")
+      .makeHoldable()
+      .withSize(1)
+      .withAliases("pope")
+      .build();
+    expect(pipe.name).toBe("pipe");
+    expect(pipe.description).toBe("This is not a pipe");
+    expect(pipe.holdable).toBe(true);
+    expect(pipe.size).toBe(1);
+    expect(pipe.aliases).toInclude("pope");
+  });
 
-test("items can be built with a builder", () => {
-  const pipe = new Item.Builder()
-    .withName("pipe")
-    .withDescription("This is not a pipe")
-    .makeHoldable()
-    .withSize(1)
-    .withAliases("pope")
-    .build();
-  expect(pipe.name).toBe("pipe");
-  expect(pipe.description).toBe("This is not a pipe");
-  expect(pipe.holdable).toBe(true);
-  expect(pipe.size).toBe(1);
-  expect(pipe.aliases).toInclude("pope");
-});
-
-test("items built with a builder have the correct verbs", () => {
-  const pipe = new Item.Builder().withName("pipe").makeHoldable().withVerbs(new Verb("smoke")).build();
-  expect(pipe.name).toBe("pipe");
-  expect(pipe.holdable).toBe(true);
-  expect(pipe.getVerb("examine")).not.toBeUndefined();
-  expect(pipe.getVerb("take")).not.toBeUndefined();
-  expect(pipe.getVerb("smoke")).not.toBeUndefined();
+  test("items built with a builder have the correct verbs", () => {
+    const pipe = new Item.Builder().withName("pipe").makeHoldable().withVerbs(new Verb("smoke")).build();
+    expect(pipe.name).toBe("pipe");
+    expect(pipe.holdable).toBe(true);
+    expect(pipe.getVerb("examine")).not.toBeUndefined();
+    expect(pipe.getVerb("take")).not.toBeUndefined();
+    expect(pipe.getVerb("smoke")).not.toBeUndefined();
+  });
 });
 
 describe("putting items", () => {
