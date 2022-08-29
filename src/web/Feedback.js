10e2a3fb20048bd8ac02f1e0861a013a88c28b2f
Rich Locke <virelsa@pilchardo.com>
2022-08-29 07:59:49 +0100

    Bump react-scroll, start on feedback popover

--- src/web/Feedback.js
+++ src/web/Feedback.js
@@ -1,11 +1,52 @@
-import { Button } from "@mui/material";
-import React from "react";
+import { Button, Card, CardActions, CardContent, Paper, Popover, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
 
-const Feedback = (props) => {
+const Feedback = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "feedback-popover" : undefined;
+
   return (
-    <Button variant="outlined" color="secondary" sx={{ marginBottom: "8px" }}>
-      Feedback
-    </Button>
+    <>
+      <Button variant="outlined" color="secondary" sx={{ marginBottom: "8px" }} onClick={handleClick}>
+        Feedback
+      </Button>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right"
+        }}
+        transformOrigin={{
+          vertical: "bottom",
+          horizontal: "right"
+        }}
+      >
+        <Card variant="outlined" sx={{ width: 400 }}>
+          <CardContent sx={{ p: 1 }}>
+            <Typography>Spotted a bug? Got a suggestion? Let me know about it:</Typography>
+            <TextField multiline placeholder="I saw a bug when..." rows={4} variant="outlined" fullWidth />
+          </CardContent>
+          <CardActions>
+            <Button variant="outlined" color="secondary">
+              Submit
+            </Button>
+          </CardActions>
+        </Card>
+      </Popover>
+    </>
   );
 };
 
